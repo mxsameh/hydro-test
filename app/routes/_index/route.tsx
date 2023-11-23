a3154578738b563a3dc8699dbb5ccf2eb77a2602
Mohamed <mxsameh@gmail.com>
2023-11-23 14:09:18 +0200

    tst mod

---
+++ app/routes/_index/route.tsx
@@ -0,0 +1,9 @@
+import styles from './styles/styles.module.css';
+
+export default function index() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>module sameh</h1>
+    </main>
+  );
+}
